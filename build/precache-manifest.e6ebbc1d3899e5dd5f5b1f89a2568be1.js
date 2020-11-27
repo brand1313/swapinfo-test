c844b073b64919e18d46d9e7fb798c35dcea149a
Yun, Kim <brand13@naver.com>
2020-11-27 11:16:57 +0900

    fixed

---
+++ build/precache-manifest.e6ebbc1d3899e5dd5f5b1f89a2568be1.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "896b4126f44fa83a03f910464c045252",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a5a379091f04256d3b56",
+    "url": "/static/js/2.b8f9e49b.chunk.js"
+  },
+  {
+    "revision": "10b3707dc0f5734da6d87d8c8881cb54",
+    "url": "/static/js/2.b8f9e49b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4ae497c0547507c5eaa2",
+    "url": "/static/js/main.01f85f8c.chunk.js"
+  },
+  {
+    "revision": "c7805d0a662790c235f8",
+    "url": "/static/js/runtime-main.c0105f97.js"
+  },
+  {
+    "revision": "73dabb371ddce7c640334fe0904dcddf",
+    "url": "/static/media/eth.73dabb37.png"
+  },
+  {
+    "revision": "5827780d34d7c141d5e763fd7b596313",
+    "url": "/static/media/logo.5827780d.svg"
+  },
+  {
+    "revision": "edb44e56409a7af705c227bd272fac47",
+    "url": "/static/media/logo_white.edb44e56.svg"
+  },
+  {
+    "revision": "9914390fc6b153e39de552594e0b1d3a",
+    "url": "/static/media/wordmark_white.9914390f.svg"
+  }
+]);
